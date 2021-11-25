afddedb9b6a27f1a71fc79b5e7b4d30abc18e673
Nistor Gheorghe <nistorgeorge404@gmail.com>
2021-11-25 20:06:52 +0200

    upload script

---
+++ decrypt.py
@@ -0,0 +1,20 @@
+import sys
+from os.path import exists as file_exists
+
+def xor(text, key):
+    decrypted = []
+    for i in range(0, len(text)):
+        decrypted.append(text[i] ^ key[i % len(key)])
+    return bytes(decrypted)
+    
+key = sys.argv[1]
+input_file = sys.argv[2]
+output_file = sys.argv[3]
+
+if file_exists(input_file):
+    with open(input_file, "rb") as IN:
+        with open(output_file, "w") as OUT:
+            decrypted = xor(IN.read(), key.encode()).decode()
+            OUT.write(decrypted)
+else:
+    print(f"'{input_file}' doesn't exist!")
---
+++ encrypt.py
@@ -0,0 +1,20 @@
+import sys
+from os.path import exists as file_exists
+
+def xor(text, key):
+    encrypted = []
+    for i in range(0, len(text)):
+        encrypted.append(text[i] ^ key[i % len(key)])
+    return bytes(encrypted)
+   
+key = sys.argv[1]
+input_file = sys.argv[2]
+output_file = sys.argv[3]
+
+if file_exists(input_file) and input_file.lower().endswith('.txt'):
+    with open(input_file, "r") as IN:
+        with open(output_file, "wb") as OUT:
+            encrypted = xor(IN.read().encode(), key.encode())
+            OUT.write(encrypted)
+else:
+    print(f"'{input_file}' doesn't exist or is not a text file!")
