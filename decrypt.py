58806fb4139ff8d5e1ab6dd28d969669747d9b47
Nistor Gheorghe <nistorgeorge404@gmail.com>
2021-11-29 14:37:20 +0200

    params error & new key

--- decrypt.py
+++ decrypt.py
@@ -6,15 +6,18 @@ def xor(text, key):
     for i in range(0, len(text)):
         decrypted.append(text[i] ^ key[i % len(key)])
     return bytes(decrypted)
-    
-key = sys.argv[1]
-input_file = sys.argv[2]
-output_file = sys.argv[3]
 
-if file_exists(input_file):
-    with open(input_file, "rb") as IN:
-        with open(output_file, "w") as OUT:
-            decrypted = xor(IN.read(), key.encode()).decode()
-            OUT.write(decrypted)
+if len(sys.argv) == 4:
+    key = sys.argv[1]
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    if file_exists(input_file):
+        with open(input_file, "rb") as IN:
+            with open(output_file, "w") as OUT:
+                decrypted = xor(IN.read(), key.encode()).decode()
+                OUT.write(decrypted)
+    else:
+        print(f"'{input_file}' doesn't exist!")
 else:
-    print(f"'{input_file}' doesn't exist!")
+	print("usage: $python3 decrypt.py <key> <input> <output.txt>")
--- encrypt.py
+++ encrypt.py
@@ -7,14 +7,17 @@ def xor(text, key):
         encrypted.append(text[i] ^ key[i % len(key)])
     return bytes(encrypted)
    
-key = sys.argv[1]
-input_file = sys.argv[2]
-output_file = sys.argv[3]
+if len(sys.argv) == 4:   
+    key = sys.argv[1]
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
 
-if file_exists(input_file) and input_file.lower().endswith('.txt'):
-    with open(input_file, "r") as IN:
-        with open(output_file, "wb") as OUT:
-            encrypted = xor(IN.read().encode(), key.encode())
-            OUT.write(encrypted)
+    if file_exists(input_file) and input_file.lower().endswith(('.txt', '.in')):
+        with open(input_file, "r") as IN:
+            with open(output_file, "wb") as OUT:
+                encrypted = xor(IN.read().encode(), key.encode())
+                OUT.write(encrypted)
+    else:
+        print(f"'{input_file}' doesn't exist or is not a text file!")
 else:
-    print(f"'{input_file}' doesn't exist or is not a text file!")
+	print("usage: $python3 encrypt.py <key> <input.txt> <output>")
